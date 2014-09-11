de7deb28f92ffb7b0a2e4e028d4c84c6e4ee0e80
soon530 <soon530@gmail.com>
2014-09-12 01:06:30 +0800

    可以幫書本新增任意的tag OPEN - #10: 身為[會員]，我可以在我收藏書的時候，新增任意標籤 https://github.com/soon530/Bukr/issues/issue/10

--- src/co/bukr/TagActivity.java
+++ src/co/bukr/TagActivity.java
@@ -11,7 +11,6 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
@@ -37,6 +36,8 @@ public class TagActivity extends Activity implements OnQueryTextListener {
     private ArrayAdapter<String> mAdapter;
 	protected ArrayList<String> mTags = new ArrayList<String>();
 	protected ArrayList<String> mFgID = new ArrayList<String>();
+	private TextView mBookTagsShow;
+	private String mBookTags;
 
 
 	@Override
@@ -48,6 +49,7 @@ public class TagActivity extends Activity implements OnQueryTextListener {
 		
         mSearchView = (SearchView) findViewById(R.id.search_view);
         mListView = (ListView) findViewById(R.id.list_view);
+        mBookTagsShow = (TextView) findViewById(R.id.book_tags);
         setupSearchView();
 
         mListView.setOnItemClickListener(new OnItemClickListener() {
@@ -55,10 +57,16 @@ public class TagActivity extends Activity implements OnQueryTextListener {
 			@Override
 			public void onItemClick(AdapterView<?> parent, View view,
 					int position, long id) {
-				TextView textView = (TextView) view.findViewById(android.R.id.text1);
-
-		        textView.setTextColor(getResources().getColor(R.color.Bukr));
 				
+				String fgID = mFgID.get(position);
+				Log.i(LOG_TAG, "mBookTags:" + mBookTags + ", mTags.get():" + mTags.get(position) + ", contains:" + mBookTags.contains(mTags.get(position)));
+				//如果書本中，有這個分類，就將這個分類刪除
+				if (mBookTags.contains(mTags.get(position))) {
+					delBook(fgID);
+				} else { //如果書本中，沒有這個分類，就將這個分類增加
+					addBook(fgID);
+				}
+				getBookTags();
 			}
 		});
         
@@ -69,8 +77,10 @@ public class TagActivity extends Activity implements OnQueryTextListener {
 					int position, long id) {
 				
 				delTag(mFgID.get(position));
-				
-				return false;
+			
+				// true		不會再call item click
+				// false	會再去call item click
+				return true;
 			}
 		});
 		
@@ -80,10 +90,92 @@ public class TagActivity extends Activity implements OnQueryTextListener {
 		} catch (Exception e) {
 		}
 
+		getBookTags();
 		showTags();
 	}
 
-    private void setupSearchView() {
+    private void getBookTags() {
+    	mBookTags = "";
+		ReqUtil.send("Bookcase/tag/contains/" + Config.bkID, null, new COIMCallListener() {
+
+
+			@Override
+			public void onSuccess(JSONObject result) {
+				Log.i(LOG_TAG, "success: "+result);
+				JSONArray jsonBooks  = Assist.getList(result);
+				for(int i = 0; i < jsonBooks.length(); i++)  {
+					JSONObject jsonBook;
+					
+					try {
+						jsonBook = (JSONObject) jsonBooks.get(i);
+						Log.i(LOG_TAG, "title: " + jsonBook.getString("title"));
+						
+						String title = jsonBook.getString("title");
+						mBookTags = mBookTags + " " + title;
+						
+					} catch (JSONException e) {
+						e.printStackTrace();
+					}
+					
+				}
+
+				mBookTagsShow.setText(mBookTags);
+			}
+			
+			
+			public void onFail(HttpResponse response, Exception exception) {
+				Log.i(LOG_TAG, "fail: "+ exception.getLocalizedMessage());
+				
+			}
+		});
+
+		
+	}
+
+
+	protected void delBook(String fgID) {
+		Map<String, Object> mapParam = new HashMap<String, Object>();
+		mapParam.put("bkID", Config.bkID);
+
+		ReqUtil.send("Bookcase/tag/rmBook/" + fgID, mapParam, new COIMCallListener() {
+
+
+			@Override
+			public void onSuccess(JSONObject result) {
+				Log.i(LOG_TAG, "success: "+result);
+				
+			}
+			
+			public void onFail(HttpResponse response, Exception exception) {
+				Log.i(LOG_TAG, "fail: "+ exception.getLocalizedMessage());
+				
+			}
+		});
+	
+	}
+
+	protected void addBook(String fgID) {
+		Map<String, Object> mapParam = new HashMap<String, Object>();
+		mapParam.put("bkID", Config.bkID);
+
+		ReqUtil.send("Bookcase/tag/addBook/" + fgID, mapParam, new COIMCallListener() {
+
+
+			@Override
+			public void onSuccess(JSONObject result) {
+				Log.i(LOG_TAG, "success: "+result);
+				
+			}
+			
+			public void onFail(HttpResponse response, Exception exception) {
+				Log.i(LOG_TAG, "fail: "+ exception.getLocalizedMessage());
+				
+			}
+		});
+    	
+	}
+
+	private void setupSearchView() {
         mSearchView.setIconifiedByDefault(false);
         mSearchView.setOnQueryTextListener(this);
         mSearchView.setSubmitButtonEnabled(false);
@@ -128,7 +220,6 @@ public class TagActivity extends Activity implements OnQueryTextListener {
 		        mListView.setAdapter(mAdapter);
 		        mListView.setTextFilterEnabled(true);
 				
-				
 			}
 			
 			
@@ -150,7 +241,7 @@ public class TagActivity extends Activity implements OnQueryTextListener {
 				Log.i(LOG_TAG, "success: "+result);
 				
 				showTags();
-				
+				getBookTags();
 			}
 			
 			
@@ -165,7 +256,7 @@ public class TagActivity extends Activity implements OnQueryTextListener {
 	@Override
 	public boolean onQueryTextSubmit(String query) {
 		addTag(query);
-		
+		mSearchView.setQuery("", false);
 		return false;
 	}
 
