6f0d3b11493701dff13f08339a6939bab7e86161
soon530 <soon530@gmail.com>
2014-09-11 23:42:56 +0800

    可新增及搜尋tag，點一下可以變紅色 OPEN - #11: 身為[會員]，我可以編輯我藏書的標籤，任意增加、刪除 https://github.com/soon530/Bukr/issues/issue/11

--- src/co/bukr/TagActivity.java
+++ src/co/bukr/TagActivity.java
@@ -1,13 +1,9 @@
 package co.bukr;
 
 import java.util.ArrayList;
-
-import it.gmariotti.cardslib.library.internal.Card;
-import it.gmariotti.cardslib.library.internal.CardArrayAdapter;
-import it.gmariotti.cardslib.library.internal.CardHeader;
-import it.gmariotti.cardslib.library.internal.CardHeader.OnClickCardHeaderPopupMenuListener;
-import it.gmariotti.cardslib.library.internal.base.BaseCard;
-import it.gmariotti.cardslib.library.view.CardListView;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.http.HttpResponse;
 import org.json.JSONArray;
@@ -15,32 +11,55 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
-import co.bukr.BookListCard.BookGridCardHeader;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.SearchView.OnQueryTextListener;
+import android.widget.TextView;
 
 import com.coimotion.csdk.common.COIMCallListener;
 import com.coimotion.csdk.common.COIMException;
 import com.coimotion.csdk.util.Assist;
 import com.coimotion.csdk.util.ReqUtil;
 
-public class TagActivity extends Activity {
+public class TagActivity extends Activity implements OnQueryTextListener {
 	private final static String LOG_TAG = "TagActivity";
-	private CardListView mListView;
-	private CardArrayAdapter mCardArrayAdapter;
-	//protected ArrayAdapter<Card> mBookCards;
-	private ArrayList<Card> mBookCards = new ArrayList<Card>();
+    private SearchView mSearchView;
+    private ListView mListView;
+    private ArrayAdapter<String> mAdapter;
+	protected ArrayList<String> mTags = new ArrayList<String>();
 
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		getActionBar().setDisplayHomeAsUpEnabled(true);
-		setContentView(R.layout.book_card_list);
-		mListView = (CardListView) findViewById(R.id.book_card_list);
+		setContentView(R.layout.searchview_filter);
 		
+		
+        mSearchView = (SearchView) findViewById(R.id.search_view);
+        mListView = (ListView) findViewById(R.id.list_view);
+        setupSearchView();
+
+/*        mListView.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				TextView textView = (TextView) view.findViewById(android.R.id.text1);
+
+		        textView.setTextColor(Color.RED);
+				
+			}
+		});
+*/		
 		
 		try {
 			ReqUtil.initSDK(getApplication());
@@ -50,6 +69,13 @@ public class TagActivity extends Activity {
 
 		showTags();
 	}
+
+    private void setupSearchView() {
+        mSearchView.setIconifiedByDefault(false);
+        mSearchView.setOnQueryTextListener(this);
+        mSearchView.setSubmitButtonEnabled(false);
+        mSearchView.setQueryHint("搜尋tags");
+    }
 
 	public void showTags() {
 		//mBookCards.clear();
@@ -65,8 +91,8 @@ public class TagActivity extends Activity {
 			@Override
 			public void onSuccess(JSONObject result) {
 				Log.i(LOG_TAG, "success: "+result);
+				mTags.clear();
 				JSONArray jsonBooks  = Assist.getList(result);
-				
 				for(int i = 0; i < jsonBooks.length(); i++)  {
 					JSONObject jsonBook;
 					
@@ -75,59 +101,98 @@ public class TagActivity extends Activity {
 						Log.i(LOG_TAG, "title: " + jsonBook.getString("title"));
 						
 						String title = jsonBook.getString("title");
-						
-						//BookListCard bookCard = new BookListCard(TagActivity.this);
-						//bookCard.setBookItem(new BookItem(bkID, iconURI, title));
-						//bookCard.init();
-						
-						Card bookCard = new Card(getBaseContext());
-						bookCard.setTitle(title);
-
-						CardHeader header = new CardHeader(getBaseContext());
-						header.setButtonOverflowVisible(true);
-
-						header.setPopupMenu(R.menu.popup_edit,
-								new CardHeader.OnClickCardHeaderPopupMenuListener() {
-									@Override
-									public void onMenuItemClick(BaseCard card, MenuItem item) {
-										int id = item.getItemId();
-										switch (id) {
-										case R.id.card_edit:
-											//addFavorite();
-											break;
-										default:
-											break;
-										}
-									}
-								});
-
-						bookCard.addCardHeader(header);
-
-						mBookCards.add(bookCard);
+						mTags.add(title);
 						
 					} catch (JSONException e) {
 						e.printStackTrace();
 					}
 					
 				}
-				
-		        mCardArrayAdapter = new CardArrayAdapter(TagActivity.this, mBookCards);
-
-				
-		        mListView.setAdapter(mCardArrayAdapter);
-		        //mListView.setTextFilterEnabled(true);
+				Collections.reverse(mTags);
+				mAdapter = new ArrayAdapter<String>(TagActivity.this, android.R.layout.simple_list_item_1, mTags);
+		        mListView.setAdapter(mAdapter);
+		        mListView.setTextFilterEnabled(true);
 				
 				
 			}
 			
-			@Override
+			
 			public void onFail(HttpResponse response, Exception exception) {
 				Log.i(LOG_TAG, "fail: "+ exception.getLocalizedMessage());
 				
 			}
 		});
+
+	}
+	
+	private void delTag() {
+		// TODO Auto-generated method stub
 		
 	}
+
+	@Override
+	public boolean onQueryTextSubmit(String query) {
+		addTag(query);
+		
+		return false;
+	}
+
+	private void addTag(String query) {
+		Map<String, Object> mapParam = new HashMap<String, Object>();
+		mapParam.put("title", query);
+		//mapParam.put("descTx", "用來收藏自己的書");
+		mapParam.put("share", "0");
+
+		
+		ReqUtil.send("Bookcase/tag/create", mapParam, new COIMCallListener() {
+
+
+			@Override
+			public void onSuccess(JSONObject result) {
+				Log.i(LOG_TAG, "success: "+result);
+				
+				mListView.clearTextFilter();
+				showTags();
+				
+//				JSONArray jsonBooks  = Assist.getList(result);
+//				
+//				for(int i = 0; i < jsonBooks.length(); i++)  {
+//					JSONObject jsonBook;
+//					
+//					try {
+//						jsonBook = (JSONObject) jsonBooks.get(i);
+//						Log.i(LOG_TAG, "title: " + jsonBook.getString("title"));
+//						
+//						String title = jsonBook.getString("title");
+//						mTags.add(title);
+//						
+//					} catch (JSONException e) {
+//						e.printStackTrace();
+//					}
+//					
+//				}
+//				
+			}
+			
+			
+			public void onFail(HttpResponse response, Exception exception) {
+				Log.i(LOG_TAG, "fail: "+ exception.getLocalizedMessage());
+				
+			}
+		});
+
+	}
+
+	@Override
+	public boolean onQueryTextChange(String newText) {
+        if (TextUtils.isEmpty(newText)) {
+            mListView.clearTextFilter();
+        } else {
+            mListView.setFilterText(newText.toString());
+        }
+        return true;
+	}
+
 
 
 	
