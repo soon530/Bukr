0f1e83ebcc0efa6fe6b96a100fcc8a00fa10558d
soon530 <soon530@gmail.com>
2014-09-17 15:06:49 +0800

    可以替這本書新增評論 (目前沒有title) OPEN - #13: 身為[會員]，我可以瀏灠及撰寫書的評論，以便參與討論 https://github.com/soon530/Bukr/issues/issue/13

--- src/co/bukr/BookCommentActivity.java
+++ src/co/bukr/BookCommentActivity.java
@@ -1,16 +1,12 @@
 package co.bukr;
 
-import it.gmariotti.cardslib.library.internal.Card;
-import it.gmariotti.cardslib.library.internal.CardArrayAdapter;
 import it.gmariotti.cardslib.library.view.CardListView;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.http.HttpResponse;
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
@@ -19,6 +15,10 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.coimotion.csdk.common.COIMCallListener;
@@ -32,6 +32,9 @@ public class BookCommentActivity extends Activity {
 	private TextView mContent;
 	private CardListView mListView;
 
+	private Button mAdd;
+	private EditText mBody;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -39,6 +42,9 @@ public class BookCommentActivity extends Activity {
 		getActionBar().setTitle("觀看書評");
 		setContentView(R.layout.book_card_list);
 		mListView = (CardListView) findViewById(R.id.book_card_list);
+		mAdd = (Button) findViewById(R.id.add);
+		mBody = (EditText) findViewById(R.id.comment);
+		
 		try {
 			ReqUtil.initSDK(getApplication());
 		} catch (COIMException e) {
@@ -46,9 +52,86 @@ public class BookCommentActivity extends Activity {
 		}
 
 		showComments();
+		
+		mAdd.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				addComment();
+			}
+
+		});
+		
 		//mContent = (TextView) findViewById(R.id.content);
 		//mContent.setText(Html.fromHtml(Config.content));
 	}
+	
+	private void addComment() {
+		
+		Map<String, Object> mapParam = new HashMap<String, Object>();
+		mapParam.put("title", "title");
+		mapParam.put("body", mBody.getText());
+		//mapParam.put("kw", keyWord);
+
+		ReqUtil.send("books/comment/create/" + Config.bkID, mapParam,
+				new COIMCallListener() {
+
+
+					@Override
+					public void onSuccess(JSONObject result) {
+						mBody.setText("");
+						Log.i(LOG_TAG, "success: " + result);
+
+						JSONArray jsonBooks  = Assist.getList(result);
+						
+						showComments();
+//						for(int i = 0; i < jsonBooks.length(); i++)  {
+//							JSONObject jsonBook;
+//							
+//							try {
+//								jsonBook = (JSONObject) jsonBooks.get(i);
+//								//Log.i(LOG_TAG, "book: " + jsonBook);
+//
+//								Log.i(LOG_TAG, "bkID: " + jsonBook.getString("bkID"));
+//								Log.i(LOG_TAG, "iconURI: " + jsonBook.getString("iconURI"));
+//								Log.i(LOG_TAG, "title: " + jsonBook.getString("title"));
+//								
+//								String bkID = jsonBook.getString("bkID");
+//								String iconURI = jsonBook.getString("iconURI");
+//								String title = jsonBook.getString("title");
+//								
+//								BookGridCard bookCard = new BookGridCard(getBaseContext());
+//								bookCard.setBookItem(new BookItem(bkID, iconURI, title));
+//								bookCard.init();
+//								bookCards.add(bookCard);
+//								
+//
+//
+//								
+//							} catch (JSONException e) {
+//								e.printStackTrace();
+//							}
+//
+//						}
+//						
+//						CardArrayAdapter mCardAdapter = new CardArrayAdapter(getBaseContext() , bookCards);
+//
+//				        mListView.setAdapter(mCardArrayAdapter);
+					
+					}
+					
+
+					@Override
+					public void onFail(HttpResponse response,
+							Exception exception) {
+						Log.i(LOG_TAG,
+								"fail: " + exception.getLocalizedMessage());
+
+					}
+				});
+
+	}
+
 	
 	private void showComments() {
 		//final ArrayList<Card> bookCards = new ArrayList<Card>();
@@ -130,6 +213,9 @@ public class BookCommentActivity extends Activity {
 		switch (id) {
 		case android.R.id.home:
 			finish();
+			break;
+		case R.id.action_add:
+			
 			break;
 		default:
 			break;
