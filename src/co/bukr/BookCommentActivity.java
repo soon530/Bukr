e0ccdd65de9b51799c599351d302e2cf93a3dc05
soon530 <soon530@gmail.com>
2014-09-17 14:16:41 +0800

    加入 新增 按鈕 OPEN - #13: 身為[會員]，我可以瀏灠及撰寫書的評論，以便參與討論 https://github.com/soon530/Bukr/issues/issue/13

--- src/co/bukr/BookCommentActivity.java
+++ src/co/bukr/BookCommentActivity.java
@@ -2,8 +2,13 @@ package co.bukr;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.text.Html;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 public class BookCommentActivity extends Activity {
@@ -15,11 +20,19 @@ public class BookCommentActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		getActionBar().setDisplayHomeAsUpEnabled(true);
 		getActionBar().setTitle("觀看書評");
-		setContentView(R.layout.activity_try);
+		setContentView(R.layout.book_card_grid);
 		
-		mContent = (TextView) findViewById(R.id.content);
-		mContent.setText(Html.fromHtml(Config.content));
+		//mContent = (TextView) findViewById(R.id.content);
+		//mContent.setText(Html.fromHtml(Config.content));
 	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.comment, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
 	
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
