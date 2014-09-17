b2bebe4ffde621b6d29f18ffa097d4011bddc09d
soon530 <soon530@gmail.com>
2014-09-17 14:38:16 +0800

    取得Bukr書評 OPEN - #13: 身為[會員]，我可以瀏灠及撰寫書的評論，以便參與討論 https://github.com/soon530/Bukr/issues/issue/13

--- src/co/bukr/BookCommentActivity.java
+++ src/co/bukr/BookCommentActivity.java
@@ -1,31 +1,120 @@
 package co.bukr;
 
+import it.gmariotti.cardslib.library.internal.Card;
+import it.gmariotti.cardslib.library.internal.CardArrayAdapter;
+import it.gmariotti.cardslib.library.view.CardListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.http.HttpResponse;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.view.MenuItemCompat;
-import android.text.Html;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.SearchView;
 import android.widget.TextView;
 
+import com.coimotion.csdk.common.COIMCallListener;
+import com.coimotion.csdk.common.COIMException;
+import com.coimotion.csdk.util.Assist;
+import com.coimotion.csdk.util.ReqUtil;
+
 public class BookCommentActivity extends Activity {
+	private final static String LOG_TAG = "SearchActivity";
 
 	private TextView mContent;
+	private CardListView mListView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		getActionBar().setDisplayHomeAsUpEnabled(true);
 		getActionBar().setTitle("觀看書評");
-		setContentView(R.layout.book_card_grid);
-		
+		setContentView(R.layout.book_card_list);
+		mListView = (CardListView) findViewById(R.id.book_card_list);
+		try {
+			ReqUtil.initSDK(getApplication());
+		} catch (COIMException e) {
+		} catch (Exception e) {
+		}
+
+		showComments();
 		//mContent = (TextView) findViewById(R.id.content);
 		//mContent.setText(Html.fromHtml(Config.content));
 	}
 	
+	private void showComments() {
+		//final ArrayList<Card> bookCards = new ArrayList<Card>();
+
+		//Map<String, Object> mapParam = new HashMap<String, Object>();
+		//mapParam.put("_ps", "12");
+		//mapParam.put("pubName", keyWord);
+		//mapParam.put("kw", keyWord);
+		
+		ReqUtil.send("books/comment/list/" + Config.bkID, null,
+				new COIMCallListener() {
+
+
+					@Override
+					public void onSuccess(JSONObject result) {
+						Log.i(LOG_TAG, "success: " + result);
+
+						JSONArray jsonBooks  = Assist.getList(result);
+												
+//						for(int i = 0; i < jsonBooks.length(); i++)  {
+//							JSONObject jsonBook;
+//							
+//							try {
+//								jsonBook = (JSONObject) jsonBooks.get(i);
+//								//Log.i(LOG_TAG, "book: " + jsonBook);
+//
+//								Log.i(LOG_TAG, "bkID: " + jsonBook.getString("bkID"));
+//								Log.i(LOG_TAG, "iconURI: " + jsonBook.getString("iconURI"));
+//								Log.i(LOG_TAG, "title: " + jsonBook.getString("title"));
+//								
+//								String bkID = jsonBook.getString("bkID");
+//								String iconURI = jsonBook.getString("iconURI");
+//								String title = jsonBook.getString("title");
+//								
+//								BookGridCard bookCard = new BookGridCard(getBaseContext());
+//								bookCard.setBookItem(new BookItem(bkID, iconURI, title));
+//								bookCard.init();
+//								bookCards.add(bookCard);
+//								
+//
+//
+//								
+//							} catch (JSONException e) {
+//								e.printStackTrace();
+//							}
+//
+//						}
+//						
+//						CardArrayAdapter mCardAdapter = new CardArrayAdapter(getBaseContext() , bookCards);
+//
+//				        mListView.setAdapter(mCardArrayAdapter);
+					
+					}
+					
+
+					@Override
+					public void onFail(HttpResponse response,
+							Exception exception) {
+						Log.i(LOG_TAG,
+								"fail: " + exception.getLocalizedMessage());
+
+					}
+				});
+		
+	}
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		MenuInflater inflater = getMenuInflater();
