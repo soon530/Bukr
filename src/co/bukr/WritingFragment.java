0132e68f9e8dce8f6cbf4f3187120346e58f57b0
soon530 <soon530@gmail.com>
2014-10-02 13:22:40 +0800

    大家正在評 導入完成 OPEN - #14: Coimotion API修改調整 https://github.com/soon530/Bukr/issues/issue/14

--- src/co/bukr/WritingFragment.java
+++ src/co/bukr/WritingFragment.java
@@ -1,44 +1,48 @@
 package co.bukr;
 
+import it.gmariotti.cardslib.library.internal.Card;
+import it.gmariotti.cardslib.library.internal.CardGridArrayAdapter;
+import it.gmariotti.cardslib.library.view.CardGridView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.http.HttpResponse;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.coimotion.csdk.common.COIMCallListener;
+import com.coimotion.csdk.util.Assist;
+import com.coimotion.csdk.util.ReqUtil;
+
+import uk.co.senab.actionbarpulltorefresh.library.ActionBarPullToRefresh;
+import uk.co.senab.actionbarpulltorefresh.library.PullToRefreshLayout;
+import uk.co.senab.actionbarpulltorefresh.library.listeners.OnRefreshListener;
 import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass. Activities that contain this fragment
- * must implement the {@link WritingFragment.OnFragmentInteractionListener}
- * interface to handle interaction events. Use the
- * {@link WritingFragment#newInstance} factory method to create an instance of
- * this fragment.
- * 
- */
-public class WritingFragment extends Fragment {
-	// TODO: Rename parameter arguments, choose names that match
-	// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-	private static final String ARG_PARAM1 = "param1";
-	private static final String ARG_PARAM2 = "param2";
+public class WritingFragment extends Fragment implements OnRefreshListener{
+	private final static String LOG_TAG = "Writing";
 
-	// TODO: Rename and change types of parameters
-	private String mParam1;
-	private String mParam2;
 
 	private OnFragmentInteractionListener mListener;
 
-	/**
-	 * Use this factory method to create a new instance of this fragment using
-	 * the provided parameters.
-	 * 
-	 * @param param1
-	 *            Parameter 1.
-	 * @param param2
-	 *            Parameter 2.
-	 * @return A new instance of fragment ReadingFragment.
-	 */
-	// TODO: Rename and change types and number of parameters
+	private ArrayList<Card> mBookCards = new ArrayList<Card>();
+	private CardGridView mGirdView;
+	private PullToRefreshLayout mPullToRefreshLayout;
+
 	public static WritingFragment newInstance(int sectionNumber) {
 		WritingFragment fragment = new WritingFragment();
 		Bundle args = new Bundle();
@@ -51,22 +55,125 @@ public class WritingFragment extends Fragment {
 		// Required empty public constructor
 	}
 
-/*	@Override
+	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		if (getArguments() != null) {
-			mParam1 = getArguments().getString(ARG_PARAM1);
-			mParam2 = getArguments().getString(ARG_PARAM2);
-		}
+		setHasOptionsMenu(true);
+
+//		if (getArguments() != null) {
+//			mParam1 = getArguments().getString(ARG_PARAM1);
+//			mParam2 = getArguments().getString(ARG_PARAM2);
+//		}
 	}
-*/
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_writing, container, false);
+		View rootView = inflater.inflate(R.layout.book_card_grid, container, false); 
+		mGirdView = (CardGridView) rootView.findViewById(R.id.book_card_grid);
+		
+		// Retrieve the PullToRefreshLayout from the content view
+		mPullToRefreshLayout = (PullToRefreshLayout) rootView
+				.findViewById(R.id.carddemo_extra_ptr_layout);
+
+		// Now setup the PullToRefreshLayout
+		 ActionBarPullToRefresh.from(getActivity())
+		 	// Mark All Children as pullable
+		 	.allChildrenArePullable()
+		 	// Set the OnRefreshListener
+			.listener(this)
+			// Finally commit the setup to our PullToRefreshLayout
+			.setup(mPullToRefreshLayout);
+
+		return rootView;
+	}
+	
+	@Override
+	public void onViewCreated(View view, Bundle savedInstanceState) {
+		showReading(false);
+		super.onViewCreated(view, savedInstanceState);
 	}
 
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+		inflater.inflate(R.menu.search, menu);
+		super.onCreateOptionsMenu(menu, inflater);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+
+		int id = item.getItemId();
+
+		switch (id) {
+		case R.id.action_search:
+			Intent intent = new Intent();
+			intent.setClass(getActivity(), SearchActivity.class);
+			startActivity(intent);
+			break;
+
+		default:
+			break;
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
+
+	private void showReading(final boolean isRefresh) {
+		
+		Map<String, Object> mapParam = new HashMap<String, Object>();
+		mapParam.put("order", "i");
+
+		ReqUtil.send("bukrBooks/book/lastCommented", mapParam, new COIMCallListener() {
+			@Override
+			public void onSuccess(JSONObject result) {
+				Log.i(LOG_TAG, "success: "+result);
+				JSONArray jsonBooks  = Assist.getList(result);
+				
+				for(int i = 0; i < jsonBooks.length(); i++)  {
+					JSONObject jsonBook;
+					
+					try {
+						jsonBook = (JSONObject) jsonBooks.get(i);
+
+						
+						String bkID = jsonBook.getString("bkID");
+						String iconUrl = BukrUtlis.getBookIconUrl(jsonBook.getString("icon"));
+						String title = jsonBook.getString("title");
+						String author = jsonBook.getString("author");
+
+						Log.i(LOG_TAG, "bkID: " + bkID);
+						Log.i(LOG_TAG, "iconUrl: " + iconUrl);
+						Log.i(LOG_TAG, "title: " + title);
+						Log.i(LOG_TAG, "author: " + author);
+						
+						BookGridCard bookCard = new BookGridCard(getActivity());
+						bookCard.setBookItem(new BookItem(bkID, iconUrl, title, author));
+						bookCard.init();
+						mBookCards.add(bookCard);
+					} catch (JSONException e) {
+						e.printStackTrace();
+					}
+					
+				}
+				
+		        CardGridArrayAdapter mCardArrayAdapter = new CardGridArrayAdapter(getActivity(), mBookCards);
+				mGirdView.setAdapter(mCardArrayAdapter);
+				
+				if (isRefresh) 
+					mPullToRefreshLayout.setRefreshComplete();
+			}
+			
+			@Override
+			public void onFail(HttpResponse response, Exception exception) {
+				Log.i(LOG_TAG, "fail: "+ exception.getLocalizedMessage());
+				
+			}
+		});
+		
+	}
+
+	
 	// TODO: Rename method, update argument and hook method into UI event
 /*	public void onButtonPressed(Uri uri) {
 		if (mListener != null) {
@@ -109,4 +216,13 @@ public class WritingFragment extends Fragment {
 		public void onFragmentInteraction(Uri uri);
 	}
 
+	
+	@Override
+	public void onRefreshStarted(View view) {
+		mBookCards.clear();
+		showReading(true);
+	}
+
 }
+
+
