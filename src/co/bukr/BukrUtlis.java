0e429ff5e507bd7d830849737e62c1aeac5e52a4
soon530 <soon530@gmail.com>
2014-10-02 13:08:06 +0800

    配合Coimotion新增取icon url的mehtod OPEN - #14: Coimotion API修改調整 https://github.com/soon530/Bukr/issues/issue/14

---
+++ src/co/bukr/BukrUtlis.java
@@ -0,0 +1,22 @@
+package co.bukr;
+
+import android.net.Uri;
+import android.util.Log;
+
+public class BukrUtlis {
+	static String getBookIconUrl(String icon) {
+		String uri = new Uri.Builder()
+				.scheme("http")
+				.authority("bukrtw.skinapi.com")
+				.appendPath("books")
+				.appendPath("auxi")
+				.appendPath("node")
+				.appendQueryParameter("path", icon)
+				.appendQueryParameter("_key", Config.COIM_APP_KEY)
+				.build()
+				.toString();
+		
+		Log.i("getBookIconUrl", uri);
+		return uri;
+	}
+}
