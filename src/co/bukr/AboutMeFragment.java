123893a40cbb7b88a106b3a0873951811d99c8c0
soon530 <soon530@gmail.com>
2014-10-22 17:38:49 +0800

    加入 FB、Email及登出功能 OPEN - #14: Coimotion API修改調整 https://github.com/soon530/Bukr/issues/issue/14

--- src/co/bukr/AboutMeFragment.java
+++ src/co/bukr/AboutMeFragment.java
@@ -1,12 +1,23 @@
 package co.bukr;
 
+import org.apache.http.HttpResponse;
+import org.json.JSONObject;
+
+import com.coimotion.csdk.common.COIMCallListener;
+import com.coimotion.csdk.util.ReqUtil;
+
 import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 /**
  * A simple {@link Fragment} subclass. Activities that contain this fragment
@@ -16,17 +27,22 @@ import android.view.ViewGroup;
  * this fragment.
  * 
  */
-public class AboutMeFragment extends Fragment {
+public class AboutMeFragment extends Fragment implements OnClickListener {
 	// TODO: Rename parameter arguments, choose names that match
 	// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 	private static final String ARG_PARAM1 = "param1";
 	private static final String ARG_PARAM2 = "param2";
+	protected static final String LOG_TAG = "AboutMeFragment";
 
 	// TODO: Rename and change types of parameters
 	private String mParam1;
 	private String mParam2;
 
 	private OnFragmentInteractionListener mListener;
+	private ImageView mFacebook;
+	private ImageView mEmail;
+	private ImageView mLogout;
+	private SharedPreferences pref;
 
 	/**
 	 * Use this factory method to create a new instance of this fragment using
@@ -63,8 +79,18 @@ public class AboutMeFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_about_me, container, false);
+		
+		View rootView = inflater.inflate(R.layout.fragment_about_me, container, false); 
+
+		mFacebook = (ImageView) rootView.findViewById(R.id.facebook);
+		mEmail = (ImageView) rootView.findViewById(R.id.email);
+		mLogout = (ImageView) rootView.findViewById(R.id.logout);
+		
+		
+		mFacebook.setOnClickListener(this);
+		mEmail.setOnClickListener(this);
+		mLogout.setOnClickListener(this);
+		return rootView;
 	}
 
 	// TODO: Rename method, update argument and hook method into UI event
@@ -108,5 +134,71 @@ public class AboutMeFragment extends Fragment {
 		// TODO: Update argument type and name
 		public void onFragmentInteraction(Uri uri);
 	}
+
+	@Override
+	public void onClick(View v) {
+		int id = v.getId();
+		switch (id) {
+		case R.id.facebook:
+			goFacebook();
+			break;
+		case R.id.email:
+			goEmail();
+			break;
+		case R.id.logout:
+			goLogout();
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	private void goFacebook() {
+		String uri = "fb://page/130253673844953";
+		Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+		startActivity(intent);
+	}
+
+	private void goEmail() {
+		Intent intent = new Intent(Intent.ACTION_SEND);
+		intent.setType("message/rfc822");
+		intent.putExtra(Intent.EXTRA_EMAIL,
+				new String[] { "service@bukr.co" });
+		intent.putExtra(Intent.EXTRA_SUBJECT, "給Bukr的建議");
+		intent.putExtra(Intent.EXTRA_TEXT, "Bukr開發小組您好： \n\n    我對Bukr的想法是...");
+		startActivity(Intent.createChooser(intent, ""));
+		
+	}
+
+	private void goLogout() {
+		ReqUtil.send("core/user/logout", null, new COIMCallListener() {
+
+			@Override
+			public void onSuccess(JSONObject result) {
+				Log.i(LOG_TAG, "result: " + result);
+				//goToHome();
+			}
+
+			@Override
+			public void onFail(HttpResponse arg0, Exception arg1) {
+				Log.i(LOG_TAG, "err: " + arg1.getLocalizedMessage());
+			}
+		});
+		
+		pref = getActivity().getApplication().getSharedPreferences("bukr", 0);
+		pref.edit().putBoolean("login", false).commit();
+		goLogin();
+	}
+	
+	private void goLogin() {
+
+		Intent intent = new Intent();
+		intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+		intent.setClass(getActivity(), LoginActivity.class);
+		startActivity(intent);
+	}
+
+
 
 }
--- src/co/bukr/LoginActivity.java
+++ src/co/bukr/LoginActivity.java
@@ -61,13 +61,7 @@ public class LoginActivity extends Activity implements OnClickListener {
 	public void onBackPressed() {
 		// TODO Auto-generated method stub
 		super.onBackPressed();
-		getApplication().getSharedPreferences("artMania", 0).edit()
-				.putBoolean("closeApp", true).commit();
 	}
-
-
-
-
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -264,52 +258,36 @@ public class LoginActivity extends Activity implements OnClickListener {
 	}
 
 	private void goToHome() {
-
 		Intent intent = new Intent();
 		intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 		intent.setClass(LoginActivity.this, MainActivity.class);
 		startActivity(intent);
 		finish();
 	}
+	
+//	@Override
+//	public boolean onCreateOptionsMenu(Menu menu) {
+//		getMenuInflater().inflate(R.menu.main, menu);
+//		return super.onCreateOptionsMenu(menu);
+//	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.main, menu);
-		return super.onCreateOptionsMenu(menu);
-	}
+//	@Override
+//	public boolean onOptionsItemSelected(MenuItem item) {
+//		int id = item.getItemId();
+//
+//		switch (id) {
+//		case R.id.action_logout:
+//			logoutBukr();
+//			break;
+//
+//		default:
+//			break;
+//		}
+//
+//		// return super.onOptionsItemSelected(item);
+//		return true;
+//	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		int id = item.getItemId();
-
-		switch (id) {
-		case R.id.action_logout:
-			logoutBukr();
-			break;
-
-		default:
-			break;
-		}
-
-		// return super.onOptionsItemSelected(item);
-		return true;
-	}
-
-	private void logoutBukr() {
-		ReqUtil.logout(new COIMCallListener() {
-
-			@Override
-			public void onSuccess(JSONObject result) {
-				Log.i(LOG_TAG, "result: " + result);
-				// goToHome();
-			}
-
-			@Override
-			public void onFail(HttpResponse arg0, Exception arg1) {
-				Log.i(LOG_TAG, "err: " + arg1.getLocalizedMessage());
-			}
-		});
-	}
 	
 }
 
